56b6f64d25fcbb313b188f2fe3611efbb5aea0a3
Divya <divyasmreeti@gmail.com>
2024-04-12 23:37:42 +0530

    latest commmit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import "./Styles.css"
 import CurrentLocation from "./Components/CurrentLocation"
+
+import {Swiper , SwiperSlide} from 'swiper/react';
+
+// import 'swiper/css/navigation';
+import { Navigation } from 'swiper';
 
 
 function App() {
@@ -10,6 +16,17 @@ function App() {
   return (
     <>
       <CurrentLocation/>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </>
   )
 }
--- src/Components/CurrentLocation.jsx
+++ src/Components/CurrentLocation.jsx
@@ -1,24 +1,43 @@
-
 import { useEffect, useState } from "react";
-import ReactAnimatedWeather from 'react-animated-weather';
+import ReactAnimatedWeather from "react-animated-weather";
 // import WeatherIcon from "react-animated-weather";
 
-
 const datebuider = (d) => {
-  let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  let day=days[d.getDay()];
-  let date=d.getDate();
-  let month=months[d.getMonth()];
-  let year=d.getFullYear();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
   return `${day} , ${date} ${month} ${year}`;
-}
+};
 
 const defaults = {
   // icon: 'CLEAR_DAY',
-  color: 'white',
+  color: "white",
   size: 80,
-  animate: true
+  animate: true,
 };
 
 const CurrentLocation = () => {
@@ -27,8 +46,8 @@ const CurrentLocation = () => {
   const [icon, setIcon] = useState("CLEAR_DAY");
   const [city, setCity] = useState("");
   const [searchedWeatherData, setSearchedWeatherData] = useState(null);
-  const apiKey = '29e86639a6fb8af5e1c95deae7b96a64';
-  const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  const apiKey = "29e86639a6fb8af5e1c95deae7b96a64";
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,14 +58,14 @@ const CurrentLocation = () => {
           const response = await fetch(
             `${baseUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
           );
-          console.log(response)
+          console.log(response);
 
           if (!response.ok) {
-            throw new Error('Weather data not available');
+            throw new Error("Weather data not available");
           }
 
           const data = await response.json();
-          console.log(data)
+          console.log(data);
           setWeatherData(data);
           switch (data.weather[0].main) {
             case "Haze":
@@ -79,7 +98,7 @@ const CurrentLocation = () => {
           }
         });
       } catch (error) {
-        console.error('Error fetching weather data:', error.message);
+        console.error("Error fetching weather data:", error.message);
       }
     };
 
@@ -93,7 +112,7 @@ const CurrentLocation = () => {
       );
 
       if (!response.ok) {
-        throw new Error('Weather data not available');
+        throw new Error("Weather data not available");
       }
 
       const data = await response.json();
@@ -104,7 +123,7 @@ const CurrentLocation = () => {
         // fetchData(coord.lat, coord.lon);
       }
     } catch (error) {
-      console.error('Error fetching weather data:', error.message);
+      console.error("Error fetching weather data:", error.message);
     }
   };
 
@@ -120,9 +139,7 @@ const CurrentLocation = () => {
 
   const formattedTime = currentTime.toLocaleTimeString();
 
-
   // CODE TO UPDATE BG ACCORDING TO DAY AND NIGHT TIME
-
 
   const updateBackground = () => {
     const currentHour = currentTime.getHours();
@@ -130,114 +147,109 @@ const CurrentLocation = () => {
     const isNightTime = currentHour >= 18 || currentHour < 6;
     const fullWrap = document.querySelector(".full-wrap");
     if (fullWrap) {
-      if(isNightTime)
-      {
+      if (isNightTime) {
         fullWrap.classList.remove("day-bg");
         fullWrap.classList.add("night-bg");
-      }
-      else
-      {
+      } else {
         fullWrap.classList.remove("night-bg");
         fullWrap.classList.add("day-bg");
       }
     }
   };
   updateBackground();
-//   useEffect(() => {
-//     const intervalID = setInterval(updateBackground, 60 * 1000);
+  //   useEffect(() => {
+  //     const intervalID = setInterval(updateBackground, 60 * 1000);
 
-// // Cleanup the interval on component unmount
-// return () => clearInterval(intervalID);
-//   })
-
-  
-
+  // // Cleanup the interval on component unmount
+  // return () => clearInterval(intervalID);
+  //   })
 
   return (
     <>
-    {weatherData ? (
-        
-          <div className="full-wrap">
-    <div className="container wrap1">
-      <div className="row row1">
-        {/* FIRST COLUMN */}
-        <div className="col-lg-6">
-          <div className="loc-details">
-        <h1 className="">{weatherData.name}, {weatherData.sys.country}</h1>
-          <span className="">{datebuider(new Date())}</span>
-          <span className="">{formattedTime}</span>
-          </div>
+      {weatherData ? (
+        <div className="full-wrap">
+          <div className="container wrap1">
+            <div className="row row1">
+              {/* FIRST COLUMN */}
+              <div className="col-lg-6">
+                <div className="loc-details">
+                  <h1 className="">
+                    {weatherData.name}, {weatherData.sys.country}
+                  </h1>
+                  <span className="">{datebuider(new Date())}</span>
+                  <span className="">{formattedTime}</span>
+                </div>
 
-          <div className="weather-details">
-            <h1>{weatherData.main.temp}</h1>
-            <div className="column-data">
-              <span>°C</span>
-              <span>{weatherData.weather[0].description}</span>
-            </div>
-            <div className="weather-icon">
-            <ReactAnimatedWeather
-              icon={icon}
-              color={defaults.color}
-              size={defaults.size}
-              animate={defaults.animate}
-            />
-            </div>
-          </div>
-        </div>
- 
-        {/*SECOND COLUMN */}
-        <div className="col-lg-6">
-        <div className="search-container">
-        <input
-        className="search-box"
-          type="text"
-          placeholder="Enter city name"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button className="search-btn" onClick={handleSearch}>Search</button>
-      </div>
-        {searchedWeatherData ? (
-              <div className="search-details">
-                <h2>
-                  {searchedWeatherData.name}, {searchedWeatherData.sys.country}
-                </h2>
-                <span>
-                  Temperature: {searchedWeatherData.main.temp} °C
-                </span>
-                <span>
-                  Weather: {searchedWeatherData.weather[0].description}
-                </span>
-                {/* <WeatherIcon
+                <div className="weather-details">
+                  <h1>{weatherData.main.temp}</h1>
+                  <div className="column-data">
+                    <span>°C</span>
+                    <span>{weatherData.weather[0].description}</span>
+                  </div>
+                  <div className="weather-icon">
+                    <ReactAnimatedWeather
+                      icon={icon}
+                      color={defaults.color}
+                      size={defaults.size}
+                      animate={defaults.animate}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {/*SECOND COLUMN */}
+              <div className="col-lg-6">
+                <div className="search-container">
+                  <input
+                    className="search-box"
+                    type="text"
+                    placeholder="Enter city name"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                  />
+                  <button className="search-btn" onClick={handleSearch}>
+                    Search
+                  </button>
+                </div>
+                {searchedWeatherData ? (
+                  <div className="search-details">
+                    <h2>
+                      {searchedWeatherData.name},{" "}
+                      {searchedWeatherData.sys.country}
+                    </h2>
+                    <span>Temperature: {searchedWeatherData.main.temp} °C</span>
+                    <span>
+                      Weather: {searchedWeatherData.weather[0].description}
+                    </span>
+                    {/* <WeatherIcon
                   icon={icon}
                   color={defaults.color}
                   size={defaults.size}
                   animate={defaults.animate}
                 /> */}
+                  </div>
+                ) : (
+                  <div className="py-3 px-1">
+                    Enter Name of any City to display its current Weather.
+                  </div>
+                )}
               </div>
-            ):(
-              <div className="py-3 px-1">
-                Enter Name of any City to display its current Weather.
-              </div>
-            )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </div>
-        
       ) : (
         <div className="loader-bg">
           <div className="canvas canvas6">
-          <div className="spinner6 p1"></div>
-          <div className="spinner6 p2"></div>
-          <div className="spinner6 p3"></div>
-          <div className="spinner6 p4"></div>
+            <div className="spinner6 p1"></div>
+            <div className="spinner6 p2"></div>
+            <div className="spinner6 p3"></div>
+            <div className="spinner6 p4"></div>
           </div>
           <p className="loader-text">Loading Weather Data ...</p>
         </div>
       )}
-    </> 
-  )
-}
+    </>
+  );
+};
 
-export default CurrentLocation
+export default CurrentLocation;
